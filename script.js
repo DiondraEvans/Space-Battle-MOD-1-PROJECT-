6572cf6bdf7e4bbb0fcd80659e1ff4555b0850f9
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-01-03 12:35:21 -0500

    made explosion

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ class Ship {
            battlePrompt.innerText = `You retreated!!!
            ${alienfleet1.ships[i].shipname} defeated you! 
            Do you want to play again?`
-            myShipImg.style.visibility = "hidden"
+            myShipImg.style.display = "none"
+            myshipimgdestroyed.style.display = "inline"
             playagainbtn.appendChild(text);
             battlePrompt.appendChild(playagainbtn)
         } else{
@@ -68,6 +69,7 @@ alienfleet1.addShip("Alien ship 6", undefined, undefined, undefined);
 //USS HelloWorld stats before playing the game
 let myShip = document.getElementById("mySpaceship")
 let myShipImg = document.getElementById("myshipimg")
+let myshipimgdestroyed = document.getElementById("myshipimgdestroyed")
 let myshiphull = document.getElementById('my-ship-hull');
 myshiphull.innerHTML = `Hull: ${me.hull}`;
 let myshipfirepower = document.getElementById('my-ship-firepower');
@@ -173,7 +175,6 @@ function playGame(){
     for( i = 0; i < alienfleet1.ships.length; i++ ){
         if (me.hull <= 0){
             wonGame = false;
-
             break;
         }
         else if (me.hull > 0){
@@ -395,174 +396,6 @@ function playGame(){
              
   
 }
-// let response = window.prompt("Want to play again?");
-// if(response.toLowerCase() == "yes"){ 
-//     window.location.reload();
-//     }
-  
-// playGame();
-// const element = document.getElementById("btn");
-// element.addEventListener("click", playGame);
-//add stats to else if statement so user knows how they killed the alien
-// for ( i = 0; i < alienfleet1.ships.length; i++){
-//     switch (alienfleet1.ships[i].shipname){
-//     case "Alien ship 1":
-//         if(alienfleet1.ships[i].hull > 0){
-//         let shiphull1 = document.getElementById("0")
-//         shiphull1.innerHTML =`Hull: ${alienfleet1.ships[i].hull}`;
-//         let shipfirepower1 = document.getElementById("firepower0")
-//         shipfirepower1.innerHTML = `Firepower: ${alienfleet1.ships[i].firepower}`;
-//         let shipaccuracy1 = document.getElementById("accuracy0")
-//         shipaccuracy1.innerHTML = `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//         }
-//         else if(alienfleet1.ships[i].hull <= 0){
-//             let alienship1 = document.getElementById("Alienship1")
-//             alienship1.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship1.innerHTML = `<b>Alienship 1 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-            
-//             alienship1.style.width = "147px";
-//             alienship1.style.height = "247px"
-//             alienship1.style.display ="flex";
-//             alienship1.style.flexDirection = "column"
-//             alienship1.style.justifyContent ="center";
-//             alienship1.style.alignItems ="center";
-//             alienship1.style.color = "white";
-           
-//             }
-//         break;
-//     case "Alien ship 2":
-//        if(alienfleet1.ships[i].hull > 0){
-//         let shiphull2 = document.getElementById("1")
-//         shiphull2.innerHTML = `Hull: ${alienfleet1.ships[i].hull}`;
-//         let shipfirepower2 = document.getElementById("firepower1")
-//         shipfirepower2.innerHTML = `Firepower: ${alienfleet1.ships[i].firepower}`;
-//         let shipaccuracy2 = document.getElementById("accuracy1")
-//         shipaccuracy2.innerHTML = `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//        } 
-//         else if(alienfleet1.ships[i].hull <= 0){
-//             let alienship2 = document.getElementById("Alienship2")
-//             alienship2.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship2.innerHTML = `<b>Alienship 2 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-//             alienship2.style.width = "147px";
-//             alienship2.style.height = "247px"
-//             alienship2.style.display ="flex";
-//             alienship2.style.flexDirection = "column"
-//             alienship2.style.justifyContent ="center";
-//             alienship2.style.alignItems ="center";
-//             alienship2.style.color = "white";
-    
-//             }
-//         break;
-//     case "Alien ship 3":
-//         if(alienfleet1.ships[i].hull > 0) {
-//             let shiphull3 = document.getElementById("2")
-//             shiphull3.innerHTML = `Hull: ${alienfleet1.ships[i].hull}`;
-//             let shipfirepower3 = document.getElementById("firepower2")
-//             shipfirepower3.innerHTML =` Firepower: ${alienfleet1.ships[i].firepower}`;
-//             let shipaccuracy3 = document.getElementById("accuracy2")
-//             shipaccuracy3.innerHTML =  `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//         }
-//         else if(alienfleet1.ships[i].hull <= 0){
-//             let alienship3 = document.getElementById("Alienship3")
-//             alienship3.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship3.innerHTML = `<b>Alienship 3 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-//             alienship3.style.width = "147px";
-//             alienship3.style.height = "247px"
-//             alienship3.style.display ="flex";
-//             alienship3.style.flexDirection = "column"
-//             alienship3.style.justifyContent ="center";
-//             alienship3.style.alignItems ="center";
-//             alienship3.style.color = "white";
-//             }
-//         break;
-//     case "Alien ship 4":
-//         if(alienfleet1.ships[i].hull > 0){
-//             let shiphull4 = document.getElementById("3")
-//             shiphull4.innerHTML = `Hull: ${alienfleet1.ships[i].hull}`;
-//             let shipfirepower4 = document.getElementById("firepower3")
-//             shipfirepower4.innerHTML = `Firepower: ${alienfleet1.ships[i].firepower}`;
-//             let shipaccuracy4 = document.getElementById("accuracy3")
-//             shipaccuracy4.innerHTML =  `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//         }
-//         else if(alienfleet1.ships[i].hull <= 0){
-//             let alienship4 = document.getElementById("Alienship4")
-//             alienship4.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship4.innerHTML = `<b>Alienship 4 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-//             alienship4.style.width = "147px";
-//             alienship4.style.height = "247px"
-//             alienship4.style.display ="flex";
-//             alienship4.style.flexDirection = "column"
-//             alienship4.style.justifyContent ="center";
-//             alienship4.style.alignItems ="center";
-//             alienship4.style.color = "white";
-//         }
-//         break;
-//     case "Alien ship 5":
-//         if(alienfleet1.ships[i].hull > 0){
-//             let shiphull5 = document.getElementById("4")
-//             shiphull5.innerHTML = `Hull: ${alienfleet1.ships[i].hull}`;
-//             let shipfirepower5 = document.getElementById("firepower4")
-//             shipfirepower5.innerHTML = `Firepower: ${alienfleet1.ships[i].firepower}`;
-//             let shipaccuracy5 = document.getElementById("accuracy4")
-//             shipaccuracy5.innerHTML =  `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//         }
-//         else if(alienfleet1.ships[i].hull <= 0){
-//             let alienship5 = document.getElementById("Alienship5")
-//             alienship5.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship5.innerHTML = `<b>Alienship 5 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-//             alienship5.style.width = "147px";
-//             alienship5.style.height = "247px"
-//             alienship5.style.display ="flex";
-//             alienship5.style.flexDirection = "column"
-//             alienship5.style.justifyContent ="center";
-//             alienship5.style.alignItems ="center";
-//             alienship5.style.color = "white";
-//         }
-//             break;
-//     case "Alien ship 6":
-//         if(alienfleet1.ships[i].hull > 0){
-//             let shiphull6 = document.getElementById("5")
-//             shiphull6.innerHTML = `Hull: ${alienfleet1.ships[i].hull}`;
-//             let shipfirepower6 = document.getElementById("firepower5")
-//             shipfirepower6.innerHTML =`Firepower: ${alienfleet1.ships[i].firepower}`
-//             let shipaccuracy6 = document.getElementById("accuracy5")
-//             shipaccuracy6.innerHTML = `Accuracy: ${alienfleet1.ships[i].accuracy}`;
-//         }
-//         else if (alienfleet1.ships[i].hull <= 0){
-//             let alienship6 = document.getElementById("Alienship6")
-//             alienship6.style.backgroundColor ="rgba(198, 22, 22, 0.257)";
-//             alienship6.innerHTML = `<b>Alienship 5 defeated</b>
-//             <br> Hull: ${alienfleet1.ships[i].hull}
-//             <br> Firepower: ${alienfleet1.ships[i].firepower}
-//             <br> Accuracy: ${alienfleet1.ships[i].accuracy}`
-//             alienship6.style.width = "147px";
-//             alienship6.style.height = "247px"
-//             alienship6.style.display ="flex";
-//             alienship6.style.flexDirection = "column"
-//             alienship6.style.justifyContent ="center";
-//             alienship6.style.alignItems ="center";
-//             alienship6.style.color = "white";
-//         }
-//         break;
-
-//     }
-// }
-
 
 //for the ship I am fighting, I want to have a battle.
 //if in battle  my hull reaches 0 I die, then I lost the game
